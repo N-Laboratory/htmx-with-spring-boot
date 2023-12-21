6fcfdd9d2737f3d6aae240a6d14a0367412db748
N-Laboratory <xnbaxx@gmail.com>
2023-12-21 21:32:22 +0900

    first commit

---
+++ src/main/java/jp/nlaboratory/MyBatisWithThreeTierArchitecture/MyBatisWithThreeTierArchitectureApplication.java
@@ -0,0 +1,13 @@
+package jp.nlaboratory.MyBatisWithThreeTierArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisWithThreeTierArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisWithThreeTierArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/nlaboratory/MyBatisWithThreeTierArchitecture/MyBatisWithThreeTierArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.nlaboratory.MyBatisWithThreeTierArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisWithThreeTierArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
