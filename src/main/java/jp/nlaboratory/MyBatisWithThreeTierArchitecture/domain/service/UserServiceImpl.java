fc73af54fdc5200f7c9b01d47021e9452adda4e1
N-Laboratory <xnbaxx@gmail.com>
2023-12-23 22:04:58 +0900

    refactor: add error handling to insert, update, delete method

--- src/main/java/jp/nlaboratory/MyBatisWithThreeTierArchitecture/domain/service/UserServiceImpl.java
+++ src/main/java/jp/nlaboratory/MyBatisWithThreeTierArchitecture/domain/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package jp.nlaboratory.MyBatisWithThreeTierArchitecture.domain.service;
 
 import jp.nlaboratory.MyBatisWithThreeTierArchitecture.application.exception.DataNotFoundException;
+import jp.nlaboratory.MyBatisWithThreeTierArchitecture.domain.dto.UserRequest;
 import jp.nlaboratory.MyBatisWithThreeTierArchitecture.domain.entity.User;
 import jp.nlaboratory.MyBatisWithThreeTierArchitecture.infrastructure.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +14,36 @@ public class UserServiceImpl implements UserService {
   private UserMapper userMapper;
 
   @Override
-  public User getUser(Long id) throws DataNotFoundException {
+  public User getUser(Long id) throws Exception {
     return userMapper.findById(id)
         .orElseThrow(() -> new DataNotFoundException("User not found. User id = " + id));
   }
 
   @Override
-  public int createUser(User user) {
-    return userMapper.insert(user);
+  public void createUser(User user) throws Exception {
+    boolean isSuccess = userMapper.insert(user);
+    if (!isSuccess) {
+      throw new Exception("An unexpected error has occurred.");
+    }
   }
 
   @Override
-  public int updateUser(User user) {
-    return userMapper.update(user);
+  public void updateUser(User user, UserRequest userRequest) throws Exception {
+    user.setName(userRequest.getName());
+    user.setEmail(userRequest.getEmail());
+    user.setDelFlg(userRequest.isDelFlg());
+
+    boolean isSuccess = userMapper.update(user);
+    if (!isSuccess) {
+      throw new Exception("An unexpected error has occurred. User id = " + user.getId());
+    }
   }
 
   @Override
-  public int deleteUser(Long id) {
-    return userMapper.delete(id);
+  public void deleteUser(Long id) throws Exception {
+    boolean isSuccess = userMapper.delete(id);
+    if (!isSuccess) {
+      throw new Exception("An unexpected error has occurred. User id = " + id);
+    }
   }
 }
