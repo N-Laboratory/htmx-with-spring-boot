032968a1f4a1646fcc75e9dc44cca01184f55cb1
N-Laboratory <xnbaxx@gmail.com>
2023-12-23 22:05:59 +0900

    refactor: get user before update, delete method

--- src/main/java/jp/nlaboratory/MyBatisWithThreeTierArchitecture/application/controller/UserController.java
+++ src/main/java/jp/nlaboratory/MyBatisWithThreeTierArchitecture/application/controller/UserController.java
@@ -6,7 +6,6 @@ import jp.nlaboratory.MyBatisWithThreeTierArchitecture.domain.dto.UserResponse;
 import jp.nlaboratory.MyBatisWithThreeTierArchitecture.domain.entity.User;
 import jp.nlaboratory.MyBatisWithThreeTierArchitecture.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,63 +20,63 @@ public class UserController {
   UserService userService;
 
   /**
-   * Search user
+   * Search user.
    *
-   * @param userRequest UserSearchRequest
-   * @param model       Model
-   * @return UserResponse
+   * @param userRequest UserSearchRequest (id)
+   * @return UserResponse (id, name, email, delFlg)
    */
   @GetMapping(value = "/search")
-  public UserResponse userSearch(@ModelAttribute UserRequest userRequest, Model model)
+  public UserResponse userSearch(@ModelAttribute UserRequest userRequest)
       throws Exception {
-    // Get user from DB by mybatis
     User user = userService.getUser(userRequest.getId());
-    return new UserResponse(user.getId(), user.getName(), user.getEmail(), user.isDelFlg(), 1);
+
+    return new UserResponse(user.getId(), user.getName(), user.getEmail(), user.isDelFlg());
   }
 
   /**
-   * Create user
+   * Create user.
    *
-   * @param userRequest UserSearchRequest
-   * @param model       Model
-   * @return UserResponse
+   * @param userRequest UserSearchRequest (name, email)
+   * @return UserResponse (id, name, email, delFlg)
    */
   @PostMapping(value = "/insert")
-  public UserResponse insert(@ModelAttribute UserRequest userRequest, Model model) {
-    // Get user from DB by mybatis
+  public UserResponse insert(@ModelAttribute UserRequest userRequest)
+      throws Exception {
     User user =
         new User(null, userRequest.getName(), userRequest.getEmail(), LocalDateTime.now(), null,
             false);
-    int count = userService.createUser(user);
-    return new UserResponse(user.getId(), user.getName(), user.getEmail(), user.isDelFlg(), count);
+    userService.createUser(user);
+
+    return new UserResponse(user.getId(), user.getName(), user.getEmail(), user.isDelFlg());
   }
 
   /**
-   * Update user
+   * Update user.
    *
-   * @param userRequest UserSearchRequest
-   * @param model       Model
-   * @return UserResponse
+   * @param userRequest UserSearchRequest (id, name, email, delFlg)
+   * @return UserResponse (id, name, email, delFlg)
    */
   @PostMapping(value = "/update")
-  public UserResponse update(@ModelAttribute UserRequest userRequest, Model model) {
-    User user = new User(userRequest.getId(), userRequest.getName(), userRequest.getEmail(), null,
-        LocalDateTime.now(), userRequest.isDelFlg());
-    // Get user from DB by mybatis
-    int count = userService.updateUser(user);
-    return new UserResponse(user.getId(), user.getName(), user.getEmail(), user.isDelFlg(), count);
+  public UserResponse update(@ModelAttribute UserRequest userRequest)
+      throws Exception {
+    User user = userService.getUser(userRequest.getId());
+    userService.updateUser(user, userRequest);
+
+    return new UserResponse(user.getId(), user.getName(), user.getEmail(), user.isDelFlg());
   }
 
   /**
-   * Delete user
+   * Delete user.
    *
-   * @param userRequest UserSearchRequest
-   * @param model       Model
+   * @param userRequest UserSearchRequest (id)
+   * @return UserResponse (id, name, email, delFlg)
    */
   @PostMapping(value = "/delete")
-  public UserResponse delete(@ModelAttribute UserRequest userRequest, Model model) {
-    // Get user from DB by mybatis
-    int count = userService.deleteUser(userRequest.getId());
-    return new UserResponse(null, null, null, null, count);
+  public UserResponse delete(@ModelAttribute UserRequest userRequest)
+      throws Exception {
+    User user = userService.getUser(userRequest.getId());
+    userService.deleteUser(user.getId());
+
+    return new UserResponse(user.getId(), user.getName(), user.getEmail(), true);
   }
 }
