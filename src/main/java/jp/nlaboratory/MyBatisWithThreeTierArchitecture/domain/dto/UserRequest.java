e91b0ac4c66099fc9df9e5f3fcab68a5aaf3b56a
N-Laboratory <xnbaxx@gmail.com>
2023-12-25 20:57:59 +0900

    refactor: modify user request dto.

--- src/main/java/jp/nlaboratory/MyBatisWithThreeTierArchitecture/domain/dto/UserRequest.java
+++ src/main/java/jp/nlaboratory/MyBatisWithThreeTierArchitecture/domain/dto/UserRequest.java
@@ -1,19 +1,20 @@
 package jp.nlaboratory.MyBatisWithThreeTierArchitecture.domain.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * Data request object.
+ * Data request object. for user search/delete.
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequest {
 
+  @NotNull(message = "Id is required.")
+  @Positive(message = "Id must be positive number.")
   private Long id;
-  private String name;
-  private String email;
-  private boolean delFlg;
 }
