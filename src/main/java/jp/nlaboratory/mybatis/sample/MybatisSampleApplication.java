5a5be01ac87e0b87345fa44e30eac7bda095df02
N-Laboratory <xnbaxx@gmail.com>
2023-12-29 21:01:49 +0900

    docs: add application description

--- src/main/java/jp/nlaboratory/mybatis/sample/MybatisSampleApplication.java
+++ src/main/java/jp/nlaboratory/mybatis/sample/MybatisSampleApplication.java
@@ -1,8 +1,16 @@
 package jp.nlaboratory.mybatis.sample;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application main class.
+ */
+@OpenAPIDefinition(info = @Info(title = "Spring Boot 3 (3-tier architecture) with MyBatis",
+    version = "1.0.0", description = "Sample project for spring boot 3 with MyBatis. "
+    + "This project implement the following. Search/Create/Update/Delete user."))
 @SpringBootApplication
 public class MybatisSampleApplication {
 
