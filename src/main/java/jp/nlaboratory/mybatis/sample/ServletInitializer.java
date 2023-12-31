f8e2147f2c29c7b94a7575defef61cc22eb26093
N-Laboratory <xnbaxx@gmail.com>
2023-12-31 18:17:56 +0900

    docs: add javadoc to ServletInitializer.java

--- src/main/java/jp/nlaboratory/mybatis/sample/ServletInitializer.java
+++ src/main/java/jp/nlaboratory/mybatis/sample/ServletInitializer.java
@@ -3,6 +3,9 @@ package jp.nlaboratory.mybatis.sample;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * ServletInitializer.
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
   @Override
