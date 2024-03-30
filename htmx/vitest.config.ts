c89d63d62bca14d55140c975c5a51d16f1de132c
N-Laboratory <xnbaxx@gmail.com>
2024-03-30 17:29:09 +0900

    chore: install puppeteer and vitest

---
+++ htmx/vitest.config.ts
@@ -0,0 +1,15 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  }
+})
