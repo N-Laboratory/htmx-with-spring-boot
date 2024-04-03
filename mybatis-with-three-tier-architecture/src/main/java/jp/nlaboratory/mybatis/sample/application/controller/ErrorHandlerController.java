0734ecb2ff6116c232f49237e42a03c841fdf518
N-Laboratory <xnbaxx@gmail.com>
2024-04-03 21:08:22 +0900

    refactor: add handleMethodArgumentTypeMismatchException to ErrorHandlerController

--- mybatis-with-three-tier-architecture/src/main/java/jp/nlaboratory/mybatis/sample/application/controller/ErrorHandlerController.java
+++ mybatis-with-three-tier-architecture/src/main/java/jp/nlaboratory/mybatis/sample/application/controller/ErrorHandlerController.java
@@ -5,17 +5,23 @@ import jp.nlaboratory.mybatis.sample.application.exception.DataNotFoundException
 import jp.nlaboratory.mybatis.sample.application.exception.InvalidParameterException;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.MyBatisSystemException;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * Handle exception.
  */
-@RestControllerAdvice
+@ControllerAdvice
 @Slf4j
 public class ErrorHandlerController extends ResponseEntityExceptionHandler {
 
@@ -23,64 +29,72 @@ public class ErrorHandlerController extends ResponseEntityExceptionHandler {
    * Create HTTP response for DataNotFoundException.
    *
    * @param e DataNotFoundException
-   * @return HTTP response (HttpStatus.BAD_REQUEST + error message)
+   * @return error page
    */
   @ExceptionHandler({DataNotFoundException.class})
-  public ResponseEntity<Object> handleDataNotFoundException(DataNotFoundException e) {
+  public String handleDataNotFoundException(DataNotFoundException e, Model model) {
     log.error("Error: {}", e.getMessage());
-    return ResponseEntity.status(HttpStatus.NOT_FOUND)
-        .body(e.getMessage());
+    model.addAttribute("errorMsg", "User not found.");
+    return "modal/error";
   }
 
   /**
    * Create HTTP response for ConnectException, CannotGetJdbcConnectionException.
    *
    * @param e ConnectException or CannotGetJdbcConnectionException
-   * @return HTTP response (HttpStatus.INTERNAL_SERVER_ERROR + error message)
+   * @return error page
    */
   @ExceptionHandler({ConnectException.class, CannotGetJdbcConnectionException.class})
-  public ResponseEntity<Object> handleConnectionException(Exception e) {
+  public String handleConnectionException(Exception e) {
     log.error("Error: {}", e.getMessage());
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(e.getMessage());
+    return "modal/error";
   }
 
   /**
    * Create HTTP response for MyBatisSystemException.
    *
    * @param e MyBatisSystemException
-   * @return HTTP response (HttpStatus.INTERNAL_SERVER_ERROR + error message)
+   * @return error page
    */
   @ExceptionHandler({MyBatisSystemException.class})
-  public ResponseEntity<Object> handleMyBatisException(MyBatisSystemException e) {
+  public String handleMyBatisException(MyBatisSystemException e) {
     log.error("Error: {}", e.getMessage());
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(e.getMessage());
+    return "modal/error";
   }
 
   /**
    * Create HTTP response for InvalidParameterException.
    *
    * @param e InvalidParameterException
-   * @return HTTP response (HttpStatus.BAD_REQUEST + error message)
+   * @return error page
    */
   @ExceptionHandler({InvalidParameterException.class})
-  public ResponseEntity<Object> handleInvalidParameterException(InvalidParameterException e) {
+  public String handleInvalidParameterException(InvalidParameterException e) {
     log.error("Error: {}", e.getMessage());
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body("Request parameter is invalid.\n" + e.getMessage());
+    return "modal/error";
   }
 
   /**
    * Create HTTP response for Exception.
    *
    * @param e Exception
-   * @return HTTP response (HttpStatus.INTERNAL_SERVER_ERROR + error message)
+   * @return error page
+   */
+  @ExceptionHandler({MethodArgumentTypeMismatchException.class})
+  public String handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+    log.error("Error: {}", e.getMessage());
+    return "modal/error";
+  }
+
+  /**
+   * Create HTTP response for Exception.
+   *
+   * @param e Exception
+   * @return error page
    */
   @ExceptionHandler({Exception.class})
-  public ResponseEntity<Object> handleException(Exception e) {
+  public String handleException(Exception e) {
     log.error("Error: {}", e.getMessage());
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body("An unexpected error has occurred. " + e.getMessage());
+    return "modal/error";
   }
 }
