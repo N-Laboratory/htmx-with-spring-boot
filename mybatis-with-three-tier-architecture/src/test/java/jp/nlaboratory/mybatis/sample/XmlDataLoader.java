ef5a17013beac2acbe8f803d063369dec49a7a44
N-Laboratory <xnbaxx@gmail.com>
2024-04-23 21:49:37 +0900

    feat: implement xml data loader for JUnit

---
+++ mybatis-with-three-tier-architecture/src/test/java/jp/nlaboratory/mybatis/sample/XmlDataLoader.java
@@ -0,0 +1,19 @@
+package jp.nlaboratory.mybatis.sample;
+
+import com.github.springtestdbunit.dataset.AbstractDataSetLoader;
+import java.io.InputStream;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.springframework.core.io.Resource;
+
+public class XmlDataLoader extends AbstractDataSetLoader {
+
+  @Override
+  protected IDataSet createDataSet(Resource resource) throws Exception {
+    FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+    builder.setColumnSensing(true);
+    try (InputStream stream = resource.getInputStream()) {
+      return builder.build(stream);
+    }
+  }
+}
