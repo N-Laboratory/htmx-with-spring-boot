8f2c2a525f3a9e4173ca9e9ca21d901def74b806
N-Laboratory <xnbaxx@gmail.com>
2024-04-27 19:18:46 +0900

    refactor: implement builder design pattern in UserUpdateRequest.java

--- mybatis-with-three-tier-architecture/src/main/java/jp/nlaboratory/mybatis/sample/domain/dto/UserUpdateRequest.java
+++ mybatis-with-three-tier-architecture/src/main/java/jp/nlaboratory/mybatis/sample/domain/dto/UserUpdateRequest.java
@@ -4,16 +4,20 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Data request object for user update.
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class UserUpdateRequest {
 
   @Schema(description = "User id", example = "1")
